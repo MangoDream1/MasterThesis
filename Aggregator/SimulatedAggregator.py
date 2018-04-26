60b15a3e5b182a71cfbea75e753a546988db1d4a
MangoDream1 <axel.verheul@gmail.com>
2018-04-26 20:49:58 +0200

    finished NSGAII; tests needed to fine tune

--- Aggregator/SimulatedAggregator.py
+++ Aggregator/SimulatedAggregator.py
@@ -3,13 +3,16 @@ from Objects.SimulatedTransaction import SimulatedTransaction
 
 
 class SimulatedAggregator(Aggregator):
-    def __init__(self, n_transactions, time_block, amount_actors, **kwargs):
+    def __init__(self, n_transactions, time_block, amount_actors,
+                 transaction_cost=100, balance_diff_multiplier=10, **kwargs):
         self._create_actors(amount_actors)
 
         transactions = [SimulatedTransaction(self.actors, **kwargs)
                         for _ in range(n_transactions)]
 
-        super().__init__(transactions, time_block)
+        super().__init__(transactions, time_block,
+                         transaction_cost=transaction_cost,
+                         balance_diff_multiplier=balance_diff_multiplier)
 
     def _create_actors(self, amount_actors):
         """
