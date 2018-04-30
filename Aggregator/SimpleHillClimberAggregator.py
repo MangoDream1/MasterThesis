181932f1cbe7a8dd7fb973eb3ea4bd90729582c7
MangoDream1 <axel.verheul@gmail.com>
2018-04-30 15:27:52 +0200

    ported simplehillclimber

---
+++ Aggregator/SimpleHillClimberAggregator.py
@@ -0,0 +1,39 @@
+import scipy as sc
+import numpy as np
+import networkx as nx
+from random import random
+
+from constants.constants import MATRIX_FORMAT
+from Aggregator.GenericAggregator import GenericAggregator
+
+class SimpleHillClimber(GenericAggregator):
+    def __init__(self, non_improvement=500, **kwargs):
+        super().__init__(**kwargs)
+
+        self.non_improvement = non_improvement
+
+    def iterate(self):
+        combined_cost = sum(self.cost(self.matrix))
+
+        non_improvement = 0
+
+        while non_improvement <= self.non_improvement and cost != 0:
+            old_matrix = self.matrix.copy()
+            self.matrix = self.mutate(self.matrix)
+
+            cons_violation, cost = sum(self.cost(self.matrix))
+            combined_cost2 = cons_violation + cost
+
+            if combined_cost2 >= combined_cost:
+                non_improvement += 1
+                self.matrix = old_matrix
+            else:
+                non_improvement = 0
+                combined_cost = combined_cost2
+
+            self.log_data.append(np.array([cost, cons_violation, combined_cost2]))
+
+        self.network = nx.from_scipy_sparse_matrix(self.matrix, create_using=nx.DiGraph())
+
+    def random_start(self):
+        self.matrix = sc.sparse.random(*self.matrix.shape, random(), MATRIX_FORMAT)
