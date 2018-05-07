ed53aef7c7b8fb1b430ac7d3b76581491deb5f95
MangoDream1 <axel.verheul@gmail.com>
2018-05-07 15:24:33 +0200

    90% decrease; split using triangle and square connections

--- Aggregator/DividedLinearAggregator.py
+++ Aggregator/DividedLinearAggregator.py
@@ -5,6 +5,7 @@ import networkx as nx
 import numpy as np
 from itertools import combinations
 import matplotlib.pyplot as plt
+from collections import deque
 
 
 class DividedLinearAggregator(GenericAggregator):
@@ -14,11 +15,14 @@ class DividedLinearAggregator(GenericAggregator):
                          **kwargs)
 
         self._iteration_points = []
+        self._end_iteration_points = []
+        self._non_improvement_points = []
 
     def iterate(self, set_generator, *generator_args):
         cons, cost = self.cost(self.matrix)
         self.log_data.append(np.array([cost, cons, cons + cost]))
         
+        non_improvement = 0
         while True:
             old_cost = cost
             i = 0
@@ -37,10 +41,20 @@ class DividedLinearAggregator(GenericAggregator):
                 i += 1
 
             self.network = nx.from_scipy_sparse_matrix(self.matrix, create_using=nx.DiGraph())
-            self._iteration_points.append(len(self.log_data))
+            self._iteration_points.append(len(self.log_data))            
 
             if old_cost == cost:
+                non_improvement += 1
+            else:
+                non_improvement = 0
+            
+            if non_improvement == 100:
                 break
+            
+            if non_improvement == 1:
+                self._non_improvement_points.append(len(self.log_data))
+
+        self._end_iteration_points.append(len(self.log_data))
             
     def get_triangles(self):
         # TODO: with recursion
@@ -51,6 +65,27 @@ class DividedLinearAggregator(GenericAggregator):
                 for y in network.neighbors(x):
                     if network.adj[y].get(node, None):
                         yield list({node, x, y}) # FIXME: always duplicate
+
+    def get_loop(self, size, found_length=10):
+        network = self.network.to_undirected()
+
+        found = deque([], found_length)
+
+        def recursion(node, start_node, loop):
+            for neighbor in network.neighbors(node):
+                if len(loop) == size and network.adj[neighbor].get(start_node, None):
+                    sorted_loop = sorted(loop)
+
+                    if sorted_loop not in found:
+                        found.append(sorted_loop)
+                        yield sorted_loop
+                elif len(loop) < size and neighbor not in loop:
+                    loop.append(neighbor)
+                    yield from recursion(neighbor, start_node, loop)
+
+        for node in network.nodes:
+            yield from recursion(node, node, [node])
+
 
     def get_connected(self, max_length):
         network = self.network.to_undirected()
@@ -69,7 +104,13 @@ class DividedLinearAggregator(GenericAggregator):
 
         for plot in plots:
             for xc in self._iteration_points:
-                plt.subplot(plot).axvline(x=xc, color='red', label=plot)
+                plt.subplot(plot).axvline(x=xc, color='red', label=plot, ymax=0.03)
+
+            for xc in self._end_iteration_points:
+                plt.subplot(plot).axvline(x=xc, color='black', label=plot, ymax=1)   
+
+            for xc in self._non_improvement_points:
+                plt.subplot(plot).axvline(x=xc, color='green', label=plot, ymax=0.04)             
 
         plt.show()
 
--- Tests/divided_linear_test.py
+++ Tests/divided_linear_test.py
@@ -1,12 +1,21 @@
 from AggregatorWrapper.simulate_aggregator_wrapper import simulate_aggregator
 from Aggregator.DividedLinearAggregator import DividedLinearAggregator
 
-wrapper = simulate_aggregator(DividedLinearAggregator, 10, None, 10)
+wrapper = simulate_aggregator(DividedLinearAggregator, 1000, None, 100)
 agg = wrapper.aggregators[0]
 
-# agg.iterate(agg.get_connected, 3)
-agg.iterate(agg.get_triangles)
-# agg.iterate(agg.all_combinations, 3)
+print(agg.cost(agg.matrix))
+
+agg.iterate(agg.get_loop, 3)
+# agg.iterate(agg.get_triangles)
+
+print(agg.cost(agg.matrix))
+
+agg.iterate(agg.get_loop, 4)
+
+# print(agg.cost(agg.matrix))
+
+# agg.iterate(agg.get_loop, 5)
 
 print(agg.cost(agg.matrix))
 
