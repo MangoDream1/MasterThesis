d4c08d0ab25e5ce6bee2b0c1905eefb35bf5554e
MangoDream1 <axel.verheul@gmail.com>
2018-05-18 17:13:37 +0200

    tx reader

--- Objects/Transaction.py
+++ Objects/Transaction.py
@@ -1,12 +1,14 @@
 class Transaction:
-    def __init__(self, to, fr, amount, timestamp):
+    def __init__(self, to, fr, amount, timestamp, block_height=0):
         self.to = to
         self.fr = fr
-        self.amount = amount
-        self.timestamp = timestamp
+        self.amount = int(amount)
+        self.timestamp = int(timestamp)
+        self.block_height = int(block_height)
 
     def get_graph_edge(self):
         return self.to, self.fr, self.amount
 
     def __str__(self):
-        return "to: {0} from: {1} amount: {2} timestamp: {3}".format(self.to, self.fr, self.amount, self.timestamp)
+        return "to: {0} from: {1} amount: {2} timestamp: {3} block_height: {4}".format(
+            self.to, self.fr, self.amount, self.timestamp, self.block_height)
---
+++ data/reader.py
@@ -0,0 +1,38 @@
+import os
+import csv
+
+from Objects.Transaction import Transaction
+from data.data_constants import SAVE_DIR
+
+def select(item_index, lbound, ubound, size):
+    selection = []
+    for block_file in os.scandir(SAVE_DIR):
+        info = block_file.name.split("_")
+
+        if info[item_index] >= str(lbound) and info[item_index] <= str(ubound):
+            selection.append(block_file.name)
+
+        if size and len(selection) >= size:
+            return selection
+
+    return selection
+
+def select_on_block_height(lbound, ubound, size):
+    return select(0, lbound, ubound, size)
+
+def select_on_timestamp(lbound, ubound, size):
+    return select(1, lbound, ubound, size)
+
+def read_lines_selection(selection):
+    for fname in selection:
+        block_number, timestamp, _, _ = fname.split("_")
+        
+        with open(os.path.join(SAVE_DIR, fname), "r") as f:
+            reader = csv.reader(f)
+
+            for line in reader:
+                yield line + [timestamp, block_number]
+
+def create_transactions(selection):
+    for line in read_lines_selection(selection):
+        yield Transaction(*line)
