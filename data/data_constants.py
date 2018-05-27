9627e8c8ae1f022f6964bafa7b607ba4ef47d086
MangoDream1 <axel.verheul@gmail.com>
2018-05-27 17:45:30 +0200

    logs stored in .log now

--- data/data_constants.py
+++ data/data_constants.py
@@ -5,7 +5,7 @@ START_BLOCK = "0000000000000000000d26984c0229c9f6962dc74db0a6d525f2f1640396f69c"
 
 FILE_PATH = os.path.dirname(os.path.realpath(__file__))
 SAVE_DIR = os.path.join(FILE_PATH, "data")
-DEBUG = os.path.join(FILE_PATH, "debug")
-ERROR = os.path.join(FILE_PATH, "debug")
+DEBUG = os.path.join(FILE_PATH, "debug.log")
+ERROR = os.path.join(FILE_PATH, "error.log")
 SAVE_NAME = os.path.join(SAVE_DIR, "%s_%s_%s_%s.csv") # height, epoch time, n_tx (not including fees, multi transactions), block_id
-LATEST_LOCATION = os.path.join(FILE_PATH, "latest")
+LATEST_LOCATION = os.path.join(FILE_PATH, "latest.log")
