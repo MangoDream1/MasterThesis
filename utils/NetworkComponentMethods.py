7c4ccf10c7a445e362886044fa6579e50677d0e9
MangoDream1 <axel.verheul@gmail.com>
2018-06-08 14:37:59 +0200

    made variable public

--- utils/NetworkComponentMethods.py
+++ utils/NetworkComponentMethods.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 
 class NetworkComponentMethods:
     def __init__(self):
-        self._component_data = defaultdict(int)
+        self.component_data = defaultdict(int)
         self.network = np.array([])
         self.matrix = np.array([])
 
@@ -31,21 +31,21 @@ class NetworkComponentMethods:
 
         name = "Loop %s" % size
         for node in network.nodes:
-            self._component_data[name] += 1
+            self.component_data[name] += 1
             yield from recursion(node, node, [node])
 
     def get_connected(self, max_length):
         network = self.network.to_undirected()
         for xnode, ynode in combinations(network.nodes, 2):
             for path in nx.all_simple_paths(network, xnode, ynode, max_length):
-                self._component_data["Connected"] += 1
+                self.component_data["Connected"] += 1
                 yield path
 
     def get_cliques(self, max_size=10):
         network = self.network.to_undirected()
         for clique in nx.find_cliques(network):
             if len(clique) <= max_size:
-                self._component_data["Clique"] += 1
+                self.component_data["Clique"] += 1
                 yield clique
 
     def get_crosses(self, min_size=5, max_size=50):
@@ -74,14 +74,11 @@ class NetworkComponentMethods:
                 
                 combination.append(oi)
 
-                while len(in_values) != 0:
+                while len(in_values) != 0 and v < 0:
                     v += in_values.pop()
                     ii = in_indexes.pop()
                     
                     combination.append(ii)
-
-                    if v > 0:
-                        break
 
                 if len(combination) > max_size:
                     break
@@ -92,18 +89,18 @@ class NetworkComponentMethods:
                     if sorted_combi not in found:
                         found.append(sorted_combi)
                         yield sorted_combi  
-                        self._component_data["Cross"] += 1
+                        self.component_data["Cross"] += 1
 
                     combination = [node]
                     v = 0
     
     def all_combinations(self, size):
         network = self.network.to_undirected()
-        self._component_data["All"] += 1  
+        self.component_data["All"] += 1  
         return combinations(network.nodes, size)
 
     def plot_bar_component_count(self):
-        names, data = zip(*sorted(list(self._component_data.items())))
+        names, data = zip(*sorted(list(self.component_data.items())))
         x_pos = list(range(len(names)))
 
         plt.bar(x_pos, data)
