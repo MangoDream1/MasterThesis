3dcc0b2a9e990b990bd48e426ab49a6923fe471f
MangoDream1 <axel.verheul@gmail.com>
2018-06-15 22:23:42 +0200

    save intermediate results

--- ThesisData/bitcoin_data.py
+++ ThesisData/bitcoin_data.py
@@ -17,7 +17,7 @@ from Objects.SimulatedTransaction import SimulatedTransaction
 
 
 def method(agg):
-    agg.iterate(agg.get_crosses)
+    agg.iterate(agg.get_crosses, 5, 20)
     for x in [3, 4, 5, 6]:
         agg.iterate(agg.get_loop, x)
 
